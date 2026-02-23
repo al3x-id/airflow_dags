e5650f0e5f3bb999f0c0cf03c0e538f2bf86653e
Alex <idowualuko58@gmail.com>
2026-02-23 13:57:31 +0100

    fix instantiate DAG correctly

--- dags/etl_dag.py
+++ dags/etl_dag.py
@@ -337,4 +337,4 @@ def daily_etl_pipeline():
   raw_datasets = extract_datasets()
   transformed_datasets = transform_datasets(raw_datasets)
   load_to_mysql(transformed_datasets)
-dag = daily_etl_pipeline()
+daily_etl_pipeline = daily_etl_pipeline()
